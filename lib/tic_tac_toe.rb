ea69378ba17f884665d955bb40174eae6cee7b9b
Meisuleen Kased <meisuleen@gmail.com>
2018-10-15 03:53:04 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,9 +39,83 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
-    if valid_move?(board, index) == true
-      return move(board, index, token) && display_board(board)
+    if valid_move?(board, index)
+      token = current_player(board)
+      move(board, index, token)
+      display_board(board)
     else  puts 'try again'
     turn(board)
+  end
+end
+
+def turn_count(board)
+  turns = 0
+  board.each do |empty_spot|
+    if empty_spot == "X" || empty_spot == "O"
+      turns += 1
+    end
+  end
+  return turns
+end
+
+def current_player(board)
+  #if the turn_count is even return "X" else return "O"
+  turns = turn_count(board)
+  if turns.even?
+    return "X"
+    else
+      return "O"
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each {|win_combination|
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
+
+    pos_1 = board[index_0]
+    pos_2 = board[index_1]
+    pos_3 = board[index_2]
+
+    if pos_1 == "X" && pos_2 == "X" && pos_3 == "X"
+      return win_combination
+    elsif pos_1 == "O" && pos_2 == "O" && pos_3 == "O"
+      return win_combination
+    end
+  }
+  return false
+end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end
+
+def draw?(board)
+  if full?(board) && !won?(board)
+    return true
+  else return false
+  end
+end
+
+def over?(board)
+  if draw?(board) || (won?(board) && full?(board)) || (won?(board) && !full?(board))
+    return true
+  end
+end
+
+def winner(board)
+  index = won?(board)
+   !index ? nil : board[index[0]]
+end
+
+def play(board)
+  until over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
   end
 end
