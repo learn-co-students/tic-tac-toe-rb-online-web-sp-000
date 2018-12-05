3cb4c26c44e7a0ec6e3013829dd0112a17cd02b0
Brian Haltiwanger <bhalt@bellsouth.net>
2018-12-05 15:05:47 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,8 +49,13 @@ def turn(board)
   puts "Please enter 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
-  charactor = "X"
-    if valid_move?(board, index) == true
+  charactor = current_player(board)
+    if position_taken?(board,index) == true
+      puts "That spot is taken, please try again"
+      display_board(board)
+      turn(board)
+    elsif
+      valid_move?(board, index) == true
       move(board, index, charactor)
       display_board(board)
     else
@@ -130,9 +135,9 @@ def play(board)
     turn(board)
   end
   if won?(board)
-    puts "Congratulations"
+    puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
-    puts "Game is a Draw"
+    puts "Cat's Game!"
   end
 end
 
