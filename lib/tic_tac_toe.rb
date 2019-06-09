3f5ce5a08749e3ab5164b165957b51e9e65befb9
Anthony Molina <antonio-molina@att.net>
2019-06-09 03:57:10 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,5 +35,92 @@ def position_taken?(board, index)
    else  
      turn(board) 
    end
+ end
+
+ def current_player(board) 
+  num_turn = turn_count(board)
+  if num_turn % 2 == 0 
+    player = "X" 
+  else 
+    player = "O" 
+  end 
+  return player
+end 
+
+def turn_count(board)
+  turn = 0  
+  board.each do |index|
+    if index == "X" || index == "O"
+      turn += 1 
+    end 
+  end 
+  return turn
+end 
+
+def won?(board)
+  WIN_COMBINATIONS.each { |win_combo|
+    
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
+    
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
+    
+    if position_1 == "X" && position_2 == "X" && position_3 == "X" 
+      return win_combo 
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O" 
+      return win_combo 
+    end 
+  } 
+  return false 
 end
+
+def full?(board)
+  board.all? {|index| index == "X" || index == "O"}
+end 
+
+def draw?(board) 
+  if !won?(board) && full?(board)
+    return true
+  else 
+    return false 
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board)
+    return true 
+  else 
+    return false 
+  end 
+end 
+
+def winner(board) 
+  index = [] 
+  index = won?(board)
+  if index == false 
+    return nil 
+  else 
+   if  board[index[0]] == "X" 
+      return "X" 
+    else 
+      return "O" 
+    end 
+  end 
+end 
  
+ def play(board)
+   until over?(board) == true 
+     turn(board)
+   end 
+   
+   if won?(board)
+     puts "Congratulations #{winner(board)}!"
+   elsif draw?(board)
+     puts "Cat's Game!"
+   end 
+ end 
+   
+   
