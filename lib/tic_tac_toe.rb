981e2462ed48c9c9d8c6cd439c4079805f8f1560
github username <github email address>
2019-08-23 21:21:44 -0400

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,10 +1,4 @@
 require 'pry'
-# Helper Method
-# def position_taken?(board, index)
-#   !(board[index].nil? || board[index] == " ")
-# end
-
-# Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [
   [0,1,2], # Top row
@@ -26,12 +20,12 @@ def display_board(board)
 end
 
 # move
-def move(board, index, player)
-  board[index] = player
-end
-
 def input_to_index(user_input)
   user_input.to_i - 1
+end
+
+def move(board, index, player)
+  board[index] = player
 end
 
 def valid_move?(board, index)
@@ -44,24 +38,28 @@ def valid_move?(board, index)
 index.between?(0,8) && !position_taken?(board, index)
 end
 
-
-
 #///////////////////////////////////////////////////
 # Method for turn
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-  if valid_move?(board, index, player)
-    move(board, index)
-    display_board(board)
-  else
+    input = gets.strip
+    index = input_to_index(input)
+    # player = current_player(board)
+      if valid_move?(board, index)
+        move(board, index, current_player(board))
+        display_board(board)
+      else
     turn(board)
   end
 end
 
+# binding.pry
 def position_taken?(board, index)
-  board[index] === 'X' || board[index] == 'O'
+  board[index] == 'X' || board[index] == 'O'
+end
+
+def turn_count(board)
+  board.count {|player| player == 'X' || player == 'O'}
 end
 
 # Method for current players move
@@ -69,9 +67,7 @@ def current_player(board)
   turn_count(board).even? ? 'X' : 'O'
 end
 
-# def position_taken?(board, index)
-#   board[index] != " " 
-# end
+
 #///////////////////////////////////////////////////
 def won?(board)
   WIN_COMBINATIONS.each do |index|
@@ -101,5 +97,19 @@ end
 def winner(board)
   if winning_combo = won?(board)
     board[winning_combo.first]
+  end
+end
+
+# Define your play method below
+def play(board)
+  # turn_num = 0
+    until over?(board)
+      turn(board)
+      # turn_num += 1
+    end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  else draw?(board)
+    puts "Cat's Game!"
   end
 end
