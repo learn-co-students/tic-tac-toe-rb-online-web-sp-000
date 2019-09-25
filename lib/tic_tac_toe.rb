1252c87d5c07feebb6b1e0b683c2151ba3eb2301
Ronda D Cowen <cowenhome5@gmail.com>
2019-09-25 21:26:47 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,34 +32,34 @@ def valid_move?(board, index)
 end
 
 def turn_count(board)
-  board.count{|token| token == "X" || token == "O"}
-  turns = 0
-  board.each do |token|
-    if token == "X" || token == "O"
-      turns += 1
+    board.count{|token| token == "X" || token == "O"}
+    turns = 0
+    board.each do |token|
+  if token == "X" || token == "O"
+    turns += 1
   end
 end
   turns
 end
   
 def move(board, position, player_token)
-  board[position] = player_token
+    board[position] = player_token
 end
 
 def current_player(board)
   if turn_count(board) % 2 == 0
     "X"
-else 
+  else 
     "O"
   end
 end
 
 def turn(board)
   puts "Please enter 1-9:"
-    user_input = gets.strip
- index = input_to_index(user_input) 
-if valid_move?(board, index) == true
-  move(board, index, current_player(board))
+  user_input = gets.strip
+    index = input_to_index(user_input) 
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
     display_board(board)
   else 
     puts "invalid"
@@ -67,10 +67,8 @@ turn(board)
   end
 end
 
-
-
 def won?(board)
-WIN_COMBINATIONS.each do |win_combo| 
+  WIN_COMBINATIONS.each do |win_combo| 
     index_1 = win_combo[0]
     index_2 = win_combo[1]
     index_3 = win_combo[2]
@@ -79,7 +77,7 @@ WIN_COMBINATIONS.each do |win_combo|
     element_2 = board[index_2]
     element_3 = board[index_3]
     
-    if element_1 == "X" && element_2 == "X" && element_3 == "X" || element_1 == "O" && element_2 == "O" && element_3 == "O"
+  if element_1 == "X" && element_2 == "X" && element_3 == "X" || element_1 == "O" && element_2 == "O" && element_3 == "O"
       return win_combo
    end
 end
@@ -91,15 +89,15 @@ def full?(board)
 end
 
 def draw?(board)
-!won?(board) && full?(board) 
+    !won?(board) && full?(board) 
 end
 
 def over?(board)
-  won?(board) || draw?(board) || full?(board)
+    won?(board) || draw?(board) || full?(board)
 end
 
 def winner(board)
- win_combo = won?(board) 
+    win_combo = won?(board) 
  if win_combo
    return board[win_combo[0]] 
  else
@@ -110,7 +108,7 @@ def winner(board)
 def play(board)
   until over?(board)
     turn(board)
-  end
+end
   if won?(board)
     puts "Congratulations #{winner(board)}!"
   elsif draw?(board)
