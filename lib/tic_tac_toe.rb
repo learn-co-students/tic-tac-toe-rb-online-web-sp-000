1a0a117dba28cdf2eddeb3da80cbb335ed088bdc
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-22 04:14:24 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,15 @@
+#display_board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+#win_combinations
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  []
+]
+  
