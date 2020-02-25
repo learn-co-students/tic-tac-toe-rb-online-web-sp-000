b7106fbf6e7115aa4cde4373a8e4504b61b59a57
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-02-25 02:15:37 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,11 +97,11 @@ end
 #winner
 def winner(board)
    if won?(board)
-      return board[won?(board)[0]]
+     return board[won?(board)[0]]
    end
 end
 
-#turn 
+#turn
 def turn(board)
    puts "Please enter 1-9:"
    user_input = gets.strip
