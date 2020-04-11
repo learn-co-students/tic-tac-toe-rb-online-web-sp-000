9e808fe125c1c605caaaa1750d16da7178f188ef
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-11 00:55:34 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,47 @@
+WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def user_input(user_input)
+  return user_input.to_i - 1
+end
+
+def move(board, index, value="X")
+  return board[index] = value
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == ""
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  else
+    false
+  end
+end
+
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    true
+  else
+    false
+  end
+end
+
+def turn(board, index)
+  puts "Please enter 1-9:"
+  user_input = gets
+  index = user_input(user_input)
+  if valid_move?(board, index)
+    move(board, index)
+  else
+    turn(board)
+  end
+end
