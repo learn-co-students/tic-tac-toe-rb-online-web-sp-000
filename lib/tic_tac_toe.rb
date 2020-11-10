21c567dd24cd486b7037d7bc8d012d2b55fabfc8
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-10 17:37:24 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,6 +2,30 @@
     #binding.pry 
     
     
+    def turn_count(board) 
+  count = 0 
+  board.each do |entry| 
+    if entry == "X" || entry == "O"
+      count +=1 
+    end 
+end 
+count 
+end 
+ 
+
+def current_player(board) 
+  count = turn_count(board)
+  if count % 2 == 0 
+      turn = "X" 
+      
+  else  
+    turn = "O" 
+  end   
+end  
+
+
+    
+    
     WIN_COMBINATIONS = [ 
       [0, 1, 2],  
       [3, 4, 5],
