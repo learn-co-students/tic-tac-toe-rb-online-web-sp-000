b7407c78b11424d5346a4b69c5c424d47cb82b39
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-25 21:53:42 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,8 +1,38 @@
+WIN_COMBINATIONS = [
+  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
+  ]
+
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, character)
+  board[index] = character
+end
+
+def position_taken?(board, index)
+!(board[index] == " " || board[index] == "" || board[index] == nil)
+end 
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board, index) == false
+    return true
+  else
+    return false 
+  end
+end
+
+def turn_count?(board)
+  count = (board.count("X") + board.count("O"))
+  return count
 end
 
