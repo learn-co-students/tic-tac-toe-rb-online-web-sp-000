1686e11095f862e968b2b9c30fc2a658fba93cd3
Christopher Black <christopher.black.2209@googlemail.com>
2021-03-26 19:18:03 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,7 +2,6 @@ WIN_COMBINATIONS = [
   [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]
   ]
 
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -100,12 +99,6 @@ def play(board)
   else
     puts "Cat's Game!"
   end 
-
-  turn(board)
-  if over?(board)
-    winner(board)
-  else
-    turn(board)
-  end
 end
+
   
