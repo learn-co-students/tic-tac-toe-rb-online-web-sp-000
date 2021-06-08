429234316e8a19490b884dc33644a161d41ba30b
github username <github email address>
2021-06-07 21:29:41 -0500

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -125,16 +125,6 @@ def winner(board)
     end
 end
 
-# until the game is over
-#take turns
-#end
- 
-#if the game was won
-#  congratulate the winner
-#else if the game was a draw
-#  tell the players it has been a draw
-#end
-
 def play(board)
     until over?(board) == true
         turn(board)
